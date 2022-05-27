6840e3256b9940a20ede58a09f3d03749d143462
diegobufa <diegobufa2615@gmail.com>
2022-05-27 07:32:46 -0300

    Errores corregidos

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,38 @@
+//Variable de eleccion
+let eleccion = "";
+
+//Funcion para saber si desea activar el simulador o no
+function simulador(){
+    
+    eleccion = prompt("Quieres utilizar el simulador debes contestar SI o NO ?").toUpperCase();
+   while(eleccion != "NO"){
+       console.log(eleccion);
+      if(eleccion === "SI"){
+          simuladorImc();
+      }else{
+          alert("No has elejido una opcion valida");
+          simulador();
+      }
+   }
+}
+
+// Llamada a la funcion
+simulador();
 
 
-let generoUsuario = prompt("Necesito saber para poder calcular tu IMC si eres Hombre o Mujer?").toLowerCase(); 
+//Funcion del simulador para el calculo del IMC
+function simuladorImc(){
+
+// Variables
+let generoUsuario= prompt("Necesito saber para poder calcular tu IMC si eres Hombre o Mujer?").toLowerCase(); 
 let alturaEnCentimetros= parseInt(prompt("Cual es tu altura en cm?"));
 let alturaEnMetros = alturaEnCentimetros / 100;
-let pesoFinal = parseInt(prompt("Cual es tu peso en kg?"));  
-let calcularImc = Math.round (pesoFinal / (alturaEnMetros **2));           
-let clasificacion;
-let edad = parseInt(prompt("Cual es tu edad? "));
-let dato = alert("Recorda la expectativa de vida en Argetina es de: 80 años para el Hombre y para la mujer es de 82 años" );
-let edadFinal;
-let obesidad1;
+let pesoFinal = parseInt(prompt("Cual es tu peso en kg?")); 
+let calcularImc = Math.round (pesoFinal / (alturaEnMetros **2)); 
+let clasificacion; 
 
 
-
-
-
-function expectativaDeVida (edadFinal, edad){
-    
-    return calculoDeVida = edadFinal - edad; 
-       
-}
-function vidaObesidad (calculoDeVida, obesidad1){
-    let vidaObesidad1 = calculoDeVida - obesidad1;
-    document.write ("La expectativa de vida segun tu condicion es de: " + (vidaObesidad1 + edad)  + " años." + "<br>");
-}
-
-
-   
-while ( generoUsuario != ""){  
-    
+// Switch para ejecutar segun el genero elejido por el usuario
     switch (generoUsuario){
 
         case "hombre":
@@ -53,21 +57,19 @@ while ( generoUsuario != ""){
                 }
                 else if (calcularImc <= 34.9 && calcularImc > 29.9) {
                     clasificacion = " Obesidad 1"; 
-                    expectativaDeVida (80, edad);
-                    vidaObesidad (calculoDeVida, 5);
+                    
                 }
                 else if ( calcularImc <= 39.9 && calcularImc > 34.9) {
                     clasificacion = "Obesidad 2";
-                    expectativaDeVida (80, edad);
-                    vidaObesidad (calculoDeVida, 6);
+                 
                 }
                 else if (calcularImc > 40){
                     clasificacion = "Obesidad 3";
-                    expectativaDeVida (80, edad);
-                    vidaObesidad (calculoDeVida, 7);
+                    
                 }
-                let repuestaHombre = "Para Hombre el calculo de IMC es: " + calcularImc + " y tu condicion es: " + clasificacion;        
-                document.write(repuestaHombre);
+                //Mostrar en pantalla el resultado
+                alert("Para Hombre el calculo de IMC es: " + calcularImc + " y tu condicion es: " + clasificacion);
+                simulador();
                
             }  
              
@@ -96,34 +98,31 @@ while ( generoUsuario != ""){
                 }
                 else if (calcularImc <= 38 && calcularImc > 29) {
                     clasificacion = " Obesidad 1";
-                    expectativaDeVida (82, edad);
-                    vidaObesidad (calculoDeVida, 5);
+                  
                 }
                 else if ( calcularImc <= 39.9 && calcularImc > 38) {
                     clasificacion = "Obesidad 2";
-                    expectativaDeVida (82, edad);
-                    vidaObesidad (calculoDeVida, 6);
+                   
                 }
                 else if (calcularImc > 40){
                     clasificacion = "Obesidad 3";
-                    expectativaDeVida (82, edad);
-                    vidaObesidad (calculoDeVida, 7);
+                    
                 
                 }
-                let repuestaMujer = " Para Mujer el  calculo de IMC es: " + calcularImc + " y tu condicion es: " + clasificacion;        
-            
-                document.write(repuestaMujer);
+                      
+                //Mostrar en pantalla el resultado
+                alert("Para Mujer el  calculo de IMC es: " + calcularImc + " y tu condicion es: " + clasificacion);
+                simulador();
             }
                
             break;
         default:
+                // Mostrar si no se ha elejido genero
                 alert("No hay un genero elejido, por favor escribe correctamente el genero. ");
             break;
 
     } 
-    
-    break;
-
+  
 }
 
  
